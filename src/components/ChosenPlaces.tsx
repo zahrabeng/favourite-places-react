63cb189ee3f8677592b4c7deb69f598aca45a47d
Zahra <bghzahra@outlook.com>
2022-03-05 23:44:31 +0000

    add images to chosen places

--- src/components/ChosenPlaces.tsx
+++ src/components/ChosenPlaces.tsx
@@ -2,14 +2,44 @@ import PlaceEntry from "./PlaceEntry";
 
 function ChosenPlaces(): JSX.Element{
     return(
+        <section>
         <div>
+            <img src = "images/tunisia.jpg" width={800} height={500} />
             <PlaceEntry
-                placeDescription={"A beautiful sunny beach"}
+                placeDescription={"A Beautiful Sunny Beach"}
                 location={"Tunisia"}
-                content={"a really lovely place to calm down, relax and have fun"}
-                lastVisited= {[12, 7, 200]}
+                content={"a really lovely place to calm down, enjoy a swim and have fun"}
             />
         </div>
+
+        <div>
+            <img src = "images/forest.png" width={800} height={500} />
+            <PlaceEntry
+                placeDescription={"Serene Forest"}
+                location={"Delamere Forest"}
+                content={"Be at one with nature in this serene forest"}
+            />
+        </div>
+
+        <div>
+            <img src = "images/paris.jpg" width={800} height={500} />
+            <PlaceEntry
+                placeDescription={"A stroll by the Eiffel Tower"}
+                location={"Paris"}
+                content={"Live your Parisian fantasy, such a magical place"}
+            />
+        </div>
+
+        <div>
+            <img src = "images/greenwich.jpg" width={800} height={500} />
+            <PlaceEntry
+                placeDescription={"Picnic at Greenwich park"}
+                location={"London"}
+                content={"My favourite place in the whole of London. Many cute doggos to watch."}
+            />
+        </div>
+
+        </section>
     )
 }
 
